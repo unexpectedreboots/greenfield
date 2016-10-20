027a8e7c0baf053cf0f42443ebdd040a1cb941c0
Karina Kinaman <kkinaman@alumni.virginia.edu>
2016-10-20 16:20:28 -0700

    (test) Add test for logging in existing user

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -24,6 +24,38 @@ describe('', function() {
         done();
       });
     });
+
+    it('Should not add duplicate username to the database', function(done) {
+      request(signUpOptions, function(err, res, body) {
+        if (err) {
+          console.log('ERROR:', err);
+        }
+        expect(res.body).to.be.undefined;
+        done();
+      });
+    });
+  });
+
+  describe('User log in', function() {
+    var signUpOptions = {
+      method: 'POST',
+      url: 'http://localhost:3000/api/login',
+      json: {
+        username: 'Phillip',
+        password: 'Phillip'
+      }
+    };
+
+    it('Should log in an existing user to the database', function(done) {
+      request(signUpOptions, function(err, res, body) {
+        if (err) {
+          console.log('ERROR:', err);
+        }
+        expect(res.body.username).to.equal('Phillip');
+        expect(res.body.password).to.not.be.null;
+        done();
+      });
+    });
   });
 
   describe('Memory creation', function() {
