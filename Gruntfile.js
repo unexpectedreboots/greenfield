31952ce27a81c33f634a32000476b67a7cf7d4aa
Karina Kinaman <kkinaman@alumni.virginia.edu>
2016-10-20 12:23:53 -0700

    (feat) Add Gruntfile and outline basic tasks

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+  
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    nodemon: {
+      dev: {
+        script: 'index.js'
+      }
+    },
+
+    // TODO: add all other file paths that should be linted
+    eslint: {
+      target: [
+        'server/**/*.js', 'index.js'
+      ]
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          // TODO: Client side file paths will go here
+        ],
+        tasks: [
+          'eslint'
+          // TODO: will we want to concat and minify??
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('server-dev', function(target) {
+    grunt.task.run(['nodemon', 'watch']);
+  });
+
+  //TODO: FILL OUT THE REST OF THESE TASKS
+
+  grunt.registerTask('test', [
+    'mochaTest'
+  ]);
+
+  grunt.registerTask('build', [
+    'eslint', 'test'
+  ]);
+
+  grunt.registerTask('upload', function(n) {
+    // if (grunt.option('prod')) {
+    // } else {
+      grunt.task.run([ 'server-dev' ]);
+    // }
+  });
+
+  grunt.registerTask('deploy', function(n) {
+    // if (grunt.option('prod')) {
+    //   grunt.task.run(['gitpush']);
+    // } else {
+    //   grunt.task.run([ 'build', 'concat', 'uglify']);
+    // }
+  });
+
+
+};
