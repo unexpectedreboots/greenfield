e77ebc201cc366521c594387d9c33b5865c128c9
Karina Kinaman <kkinaman@alumni.virginia.edu>
2016-10-24 15:21:29 -0700

    (refactor) Reconfigure routing to set up for protected/non-protected pages

--- db/controllers/userController.js
+++ db/controllers/userController.js
@@ -4,6 +4,7 @@ exports.login = function(req, res) {
   User.findOne({username: req.body.username})
     .then(function(user) {
       // TODO: WHAT HAPPENS IF USER DOESN'T EXIST
+      // console.log('USER', user);
       if (!user) {
         res.status(401).send();
       }
@@ -19,6 +20,7 @@ exports.signup = function(req, res) {
           username: req.body.username,
           password: req.body.password
         }).then(function(user) {
+          // console.log('USER CREATED', user);
           res.status(201).send(user);
         });
       } else {
