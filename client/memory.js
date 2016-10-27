76cb086a29f51416b97f23b434b0861e9b0eda9d
Karina Kinaman <kkinaman419@gmail.com>
2016-10-26 18:42:30 -0700

    (feat) User must select which tags to display upon uploading/taking new image (client-side only, does not persis)

--- client/memory.js
+++ client/memory.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,8 +7,10 @@ import {
   AlertIOS,
   AsyncStorage,
   TouchableHighlight,
-  Image
+  Image,
+  Modal
 } from 'react-native';
+import { Container, Content, Button } from 'native-base';
 
 var STORAGE_KEY = 'id_token';
 
@@ -18,7 +20,8 @@ export default class Memory extends React.Component {
     this.state = {
       image: this.props.image,
       status: 'Loading...',
-      tags: []
+      tags: [],
+      selectedTags: []
     };
   }
 
@@ -75,6 +78,21 @@ export default class Memory extends React.Component {
     });
   }
 
+  selectTag(tagName) {
+    if (this.state.selectedTags.indexOf(tagName) === -1) {
+      var newSelectedTags = this.state.selectedTags.concat(tagName);
+      this.setState({
+        selectedTags: newSelectedTags
+      });
+    }
+  }
+
+  setSelectedTags(tagArray) {
+    this.setState({
+      selectedTags: tagArray
+    });
+  }
+
   componentDidMount() {
     if (this.props.prevScene === 'Homescreen') {
       this.uploadPhoto();
@@ -86,8 +104,9 @@ export default class Memory extends React.Component {
   render() {
     return (
       <View>
+        <ModalView tags={this.state.tags} addOneTag={this.selectTag.bind(this)} tagClickFunc={this.setSelectedTags.bind(this)}/>
         <Image style={{width:200, height:200}} source={{uri: this.state.image.uri}}/>
-        <MemoryDetails status={this.state.status} tags={this.state.tags}/>
+        <MemoryDetails status={this.state.status} tags={this.state.selectedTags}/>
       </View>
     );
   }
@@ -99,6 +118,7 @@ class MemoryDetails extends React.Component {
   }
 
   render() {
+    console.log('TAGS TO RENDER', this.props.tags);
     return (
       <View>
         <Text>{this.props.status}</Text>
@@ -107,3 +127,143 @@ class MemoryDetails extends React.Component {
     );
   }
 }
+
+class ModalView extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false,
+      selectedTags: []
+    };
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  componentDidMount() {
+    this.setModalVisible(!this.state.modalVisible)
+  }
+
+  setSelectedTags(tagArray) {
+    this.setState({
+      selectedTags: tagArray
+    });
+  }
+
+  render() {
+    return (
+      <View style={{marginTop: 22}}>
+        <Modal
+          animationType={"slide"}
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+          >
+         <View style={{marginTop: 22, backgroundColor: 'rgba(255, 255, 255, 0.9)', flex: 1}}>
+            <TagList tags={this.props.tags} addOneTag={this.props.addOneTag} setTagsInModalView={this.setSelectedTags.bind(this)} tagClickFunc={this.props.tagClickFunc}/>
+            <TouchableHighlight onPress={() => {
+              // this.props.tagClickFunc(this.state.selectedTags);
+              this.setModalVisible(!this.state.modalVisible);
+            }}>
+              <Text style={{fontSize: 50, color: '#000'}}>Done</Text>
+            </TouchableHighlight>
+         </View>
+        </Modal>
+      </View>
+    );
+  }
+}
+
+class TagList extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      selectedTags: []
+    }
+  }
+
+  addTag(tag) {
+    this.setState({
+      selectedTags: this.state.selectedTags.concat(tag)
+    });
+  }
+
+  removeTag(tag) {
+    this.setState({
+      selectedTags: this.state.selectedTags.splice(this.state.selectedTags.indexOf(tag), 1)
+    });
+  }
+
+  getTags() {
+    return this.state.selectedTags;
+  }
+
+  render() {
+    return (
+      <Container style={{flex: 1, justifyContent: 'center'}}>
+        <Content>
+          {this.props.tags.map(tag => <OneTag tagName={tag}
+                                              addOneTag={this.props.addOneTag}
+                                              getTagsFromTagList={this.getTags.bind(this)} 
+                                              setTagsInModalView={this.props.setTagsInModalView} 
+                                              addToTagList={this.addTag.bind(this)} 
+                                              removeFromTagList={this.removeTag.bind(this)} 
+                                              tagClickFunc={this.props.tagClickFunc}/>)}
+        </Content>
+      </Container>
+    );
+  }
+}
+
+class OneTag extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false
+    };
+  }
+
+  select(tag) {
+    this.props.addOneTag(tag);
+    this.setState({selected: true});
+    // if (this.state.selected) {
+    //   this.props.removeFromTagList(tag);
+    // } else {
+    //   this.props.addToTagList(tag);
+    // }
+    // this.props.setTagsInModalView(this.props.getTagsFromTagList());
+    // this.setState({selected: !this.state.selected});
+  }
+
+  render() {
+    return (
+      <Button style={this.state.selected ? styles.selectedTag: styles.normalTag} onPress={ () => this.select(this.props.tagName) } bordered>
+        {this.props.tagName}
+      </Button>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  selectedTag: {
+    backgroundColor: 'lightgray'
+  },
+  normalTag: {
+    backgroundColor: '#fff'
+  }
+});
+
+          // <Button bordered> Primary </Button>
+          // <Button bordered success> Success </Button>
+          // <Button bordered info> Info </Button>
+          // <Button bordered warning> Warning </Button>
+          // <Button bordered danger> Danger </Button>
+
+
+// <TouchableHighlight onPress={() => {
+//           this.setModalVisible(true)
+//         }}>
+//           <Text>Show Modal</Text>
+//         </TouchableHighlight>
