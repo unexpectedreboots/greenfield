59beedb315e21a2c66380ae80202a05d0307c827
Bobby Wei <showmethewei@gmail.com>
2016-10-28 17:23:27 -0700

    Add product home page

--- server/index.js
+++ server/index.js
@@ -8,16 +8,13 @@ var protectedRouter = require('./routers/protected-routes');
 var app = express();
 
 // host static client files on server
-// app.use(express.static(__dirname + '/client'/* TODO: insert/change client folder path here */));
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // configure server with routes
+app.use(express.static('tagme-home-page'));
 app.use('/api/users', userRouter);
 app.use('/api/memories', protectedRouter);
-
-// require('./routers/routes')(app, express);
 
 //start listening on given port
 var port = process.env.PORT || 3000;
