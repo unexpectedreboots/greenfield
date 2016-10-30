9dc063ef29b2ea52f0fb5c85720e83c94d4b7481
Karina Kinaman <kkinaman419@gmail.com>
2016-10-30 16:58:24 -0700

    New clarifai token

--- api/clarifai.js
+++ api/clarifai.js
@@ -1,7 +1,7 @@
 var requestPromise = require('request-promise');
 
 module.exports = function(pubUrl) {
-  var clarifaiToken = '7UbpUqQ9d5YSG2EFstAN6e89i6Ttop';
+  var clarifaiToken = 'hd7WCDL60J4Pw6Ykk3avpOkVHdS2Hb';
   return requestPromise({
     uri: 'https://api.clarifai.com/v1/tag/?url=' + pubUrl, 
     method: 'GET',
