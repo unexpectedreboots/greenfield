f3829b0a9dcd3f861d5c62447ecf963b523f1448
Bobby Wei <showmethewei@gmail.com>
2016-10-31 11:27:52 -0700

    Tweak tests. All tests are passing

--- test/ApiSpec.js
+++ test/ApiSpec.js
@@ -3,11 +3,11 @@ var clarifai = require('../api/clarifai');
 var microsoft = require('../api/microsoft');
 var caption = require('../api/caption');
 
-// NOTE: These tests will fail if the endpoint response content or structure change in any way
 describe('api calls', function() {
 
   describe('clarifai should return tags', function() {
 
+    // Note: If this test fails, check to see that the Clarifai API token is still valid
     it('has an item as the first tag', function(done) {
       clarifai('http://i0.kym-cdn.com/photos/images/facebook/000/011/296/success_baby.jpg')
       .then(function(tags) {
@@ -22,6 +22,7 @@ describe('api calls', function() {
 
   });
 
+  // Note: If the below test fails, check to see that the Microsoft API tokens are still valid
   describe('microsoft should return tags', function() {
 
     it('has an item as the first tag', function(done) {
@@ -40,7 +41,7 @@ describe('api calls', function() {
 
   });
 
-  xdescribe('captioner should return caption: ', function() {
+  describe('captioner should return caption: ', function() {
     it('thinks the successkid is on a beach and angry', function(done) {
       this.timeout(5000);
       caption('https://s3-us-west-1.amazonaws.com/invalidmemories/images/02c9dce5-e903-4396-82ea-66f5892a821e-large.jpg', 
