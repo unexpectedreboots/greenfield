916571b6ed3492fba8ceed22ba1197f859409450
Christopher Tham <chriswt91@gmail.com>
2016-11-01 15:26:18 -0700

    Specify storage parameters (S3 bucket)

--- server/transloadit.js
+++ server/transloadit.js
@@ -9,5 +9,12 @@ var params = {
     use: ':original',
     robot: '/video/encode',
     preset: 'iphone'
+  },
+  store: {
+    use: 'encode',
+    robot: '/s3/store',
+    key: 'YOUR_AMAZON_S3_KEY',
+    secret: 'YOUR_AMAZON_S3_SECRET',
+    bucket: 'YOUR_AMAZON_S3_BUCKET'
   }
 };
