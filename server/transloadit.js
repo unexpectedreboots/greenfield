54d724a96856fa42b8be11000e058f0073ac0c97
Christopher Tham <chriswt91@gmail.com>
2016-11-01 16:12:50 -0700

    Implement transloadit video upload

--- server/transloadit.js
+++ server/transloadit.js
@@ -1,27 +1,30 @@
-var TransloaditClient = require('transloadit');
-var transloadit = new TransloaditClient({
-  authKey: '',
-  authSecret: ''
-});
+var transloadit = require('node-transloadit');
+
+var client = new transloadit(
+  '8a87b470a08611e6829ce1fc61bcc136',
+  'c13e43c6792aca5c9c8c1c93f012d2e91c6fc89b'
+);
 
 var params = {
-  encode: {
-    use: ':original',
-    robot: '/video/encode',
-    preset: 'iphone',
-    url: filePath
-  },
-  store: {
-    use: 'encode',
-    robot: '/s3/store',
-    key: 'YOUR_AMAZON_S3_KEY',
-    secret: 'YOUR_AMAZON_S3_SECRET',
-    bucket: 'YOUR_AMAZON_S3_BUCKET'
+  steps: {
+    ':original': {
+      robot: '/http/import',
+      url: 'http://techslides.com/demos/sample-videos/small.mp4'
+      // TODO: make dynamic
+    },
+    'store': {
+      use: ':original',
+      robot: '/s3/store',
+      key: 'AKIAJTWXU5UJG3U627AQ',
+      secret: 'Z04NHQOrWc8ti89I2wZ3fYlFi+Xgw/tRjm/DkGVO',
+      bucket: 'unexpected-reboots-videos'
+    }
   }
 };
 
-module.exports.uploadVideo = function(filePath, callback) {
-  transloadit.send(params, function(success) {
+module.exports.uploadVideo = function(callback) {
+  // TODO: add argument to accept file path
+  client.send(params, function(success) {
     callback('Success:', JSON.stringify(success));
   }, function (err) {
     callback('Error:', JSON.stringify(err));
