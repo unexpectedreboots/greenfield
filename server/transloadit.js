d2c913a82c8219a3199d8aaea708148e2709a11d
Christopher Tham <chriswt91@gmail.com>
2016-11-01 15:33:44 -0700

    Export transloadit client
    Accepts two arguments: `filePath` and `callback`

--- server/transloadit.js
+++ server/transloadit.js
@@ -8,7 +8,8 @@ var params = {
   encode: {
     use: ':original',
     robot: '/video/encode',
-    preset: 'iphone'
+    preset: 'iphone',
+    url: filePath
   },
   store: {
     use: 'encode',
@@ -17,4 +18,12 @@ var params = {
     secret: 'YOUR_AMAZON_S3_SECRET',
     bucket: 'YOUR_AMAZON_S3_BUCKET'
   }
+};
+
+module.exports.uploadVideo = function(filePath, callback) {
+  transloadit.send(params, function(success) {
+    callback('Success:', JSON.stringify(success));
+  }, function (err) {
+    callback('Error:', JSON.stringify(err));
+  });
 };
