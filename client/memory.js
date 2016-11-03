de8f2ed201abfd5fc9e74e88f2eee9fab5e27e3c
Christopher Tham <chriswt91@gmail.com>
2016-11-03 14:34:52 -0700

    Move map centering (non-dynamic)

--- client/memory.js
+++ client/memory.js
@@ -263,8 +263,8 @@ class DisplayMap extends React.Component {
       <Components.MapView
         style={styles.map}
         initialRegion={{
-          latitude: 38.78825,
-          longitude: -123.4324,
+          latitude: 37.78825,
+          longitude: -122.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421
         }} 
