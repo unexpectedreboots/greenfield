7f914ca6b56b91506fe0b06e89ed12637d0cb07e
Christopher Tham <chriswt91@gmail.com>
2016-11-03 16:46:53 -0700

    Import required modal dependencies

---
+++ client/captionModal.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  AsyncStorage,
+  Modal
+} from 'react-native';
+import { Font } from 'exponent';
+import { Container, Content, Button } from 'native-base';
+import { Ionicons } from '@exponent/vector-icons';
