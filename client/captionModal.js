10938b5b5371297b00b932af7246aa89579a18e3
Christopher Tham <chriswt91@gmail.com>
2016-11-03 19:34:17 -0700

    Fix missing `this` reference on async call

--- client/captionModal.js
+++ client/captionModal.js
@@ -43,7 +43,7 @@ export default class CaptionView extends React.Component {
 
   save() {
     // Method called for SAVE button onPress
-    updateCaption(this.state.caption);
+    this.updateCaption(this.state.caption);
     this.setState({visible: false});
   }
 
